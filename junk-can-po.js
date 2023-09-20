727b6e0ba27d81fd067c726a112ac68161fd4928
korykkb <korysb@hawaii.edu>
2023-09-20 13:07:34 -1000

    Add game function
    Copied the code for a single round 5 times into the "game" function. Prompt will not show up for some reason.

--- junk-can-po.js
+++ junk-can-po.js
@@ -18,9 +18,31 @@ console.log(randomResult)
 /* 
 */
 
-const getPlayerChoice = parseInt(prompt("Please enter Rock, Paper, or Scissors"));
+function game() {
 
+function getPlayerChoice() {
+    const playerAnswer = parseInt(prompt("Please enter Rock, Paper, or Scissors"));
 
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection === "Paper" && computerSelection === "Rock") {
+            return "You Won! Paper covers Rock";
+        } 
+        else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            return "You Won! Rocks smashes Scissors";
+        }
+        else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            return "You Won! Scissors cuts Paper";
+        }
+        else if (computerSelection === "Paper" && playerSelection === "Rock") {
+            return "You Lost! Rock gets covered by Paper";
+        }
+        else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+            return "You Lost! Scissors is smashed by Rock";
+        }
+        else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+            return "You Lost! Paper is cut by Scissors";
+        }
+    }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Paper" && computerSelection === "Rock") {
@@ -43,9 +65,99 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = getPlayerChoice;
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Won! Paper covers Rock";
+    } 
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Won! Rocks smashes Scissors";
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Won! Scissors cuts Paper";
+    }
+    else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        return "You Lost! Rock gets covered by Paper";
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "You Lost! Scissors is smashed by Rock";
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        return "You Lost! Paper is cut by Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Won! Paper covers Rock";
+    } 
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Won! Rocks smashes Scissors";
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Won! Scissors cuts Paper";
+    }
+    else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        return "You Lost! Rock gets covered by Paper";
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "You Lost! Scissors is smashed by Rock";
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        return "You Lost! Paper is cut by Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Won! Paper covers Rock";
+    } 
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Won! Rocks smashes Scissors";
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Won! Scissors cuts Paper";
+    }
+    else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        return "You Lost! Rock gets covered by Paper";
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "You Lost! Scissors is smashed by Rock";
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        return "You Lost! Paper is cut by Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Won! Paper covers Rock";
+    } 
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Won! Rocks smashes Scissors";
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Won! Scissors cuts Paper";
+    }
+    else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        return "You Lost! Rock gets covered by Paper";
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "You Lost! Scissors is smashed by Rock";
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        return "You Lost! Paper is cut by Scissors";
+    }
+}
+}
+
+const playerSelection = getPlayerChoice();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection))
+}
+
+
+
+
 
 
 
