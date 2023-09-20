64b23eae112fc6da95c9b606a4ff108ca52d31c8
korykkb <korysb@hawaii.edu>
2023-09-20 12:40:04 -1000

    Add JS for game mechanics and to create a prompt
    The player needs to enter their answer into the prompt. The if else game mechanics determine if the player or computer wins the round

--- junk-can-po.js
+++ junk-can-po.js
@@ -15,5 +15,38 @@ const randomResult = getComputerChoice();
 console.log(randomResult)
 
 
+/* 
+*/
+
+const getPlayerChoice = parseInt(prompt("Please enter Rock, Paper, or Scissors"));
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You Won! Paper covers Rock";
+    } 
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You Won! Rocks smashes Scissors";
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You Won! Scissors cuts Paper";
+    }
+    else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        return "You Lost! Rock gets covered by Paper";
+    }
+    else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "You Lost! Scissors is smashed by Rock";
+    }
+    else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        return "You Lost! Paper is cut by Scissors";
+    }
+}
+
+const playerSelection = getPlayerChoice;
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
+
+
 
 
